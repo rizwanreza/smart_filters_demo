9704885cf82630dc764f32896173fcab1af4f989
Rizwan Reza <rizwanreza@gmail.com>
2010-08-22 01:14:10 +0430

    Added criteria for columns of integer types.

--- app/models/address_book.rb
+++ app/models/address_book.rb
@@ -7,22 +7,34 @@ class AddressBook < ActiveRecord::Base
         if options[column.name.to_sym]
           case options[column.name.to_sym].keys.first
           when "contains" 
-            return AddressBook.find(:all, 
+            return find(:all, 
                         :conditions => ["#{column.name} LIKE ?", "%#{options[column.name.to_sym]["contains"]}%"])
           when "does_not_contain"
-            return AddressBook.find(:all, 
+            return find(:all, 
                         :conditions => ["#{column.name} NOT LIKE ?", "%#{options[column.name.to_sym]["does_not_contain"]}%"])
           when "is"
-            return AddressBook.find(:all, 
+            return find(:all, 
                         :conditions => ["#{column.name} = ?", "#{options[column.name.to_sym]["is"]}"])
           when "starts_with"
-            return AddressBook.find(:all, 
+            return find(:all, 
                         :conditions => ["#{column.name} LIKE ?", "#{options[column.name.to_sym]["starts_with"]}%"])
           when "ends_with"
-            return AddressBook.find(:all, 
+            return find(:all, 
                         :conditions => ["#{column.name} LIKE ?", "%#{options[column.name.to_sym]["ends_with"]}"])
+          when "equals_to"
+            return find(:all,
+                        :conditions => ["#{column.name} = ?", "#{options[column.name.to_sym]["equals_to"]}"])
+          when "greater_than"
+            return find(:all,
+                        :conditions => ["#{column.name} > ?", "#{options[column.name.to_sym]["greater_than"]}"])
+          when "less_than"
+            return find(:all,
+                        :conditions => ["#{column.name} < ?", "#{options[column.name.to_sym]["less_than"]}"])
+          when "between"
+            return find(:all,
+                        :conditions => ["#{column.name} BETWEEN ? AND ?", "#{options[column.name.to_sym]["between"].first}", "#{options[column.name.to_sym]["between"].last}"])
           else
-            return nil
+            return []
           end
         end
       end
--- spec/models/address_book_spec.rb
+++ spec/models/address_book_spec.rb
@@ -2,21 +2,26 @@ require "spec_helper"
 
 describe AddressBook do
   describe ".smart_filter" do
-    let(:bob) { Factory(:address_book, :name => "Bob Martin") }
-    let(:david) { Factory(:address_book, :name => "David Henderson") }
+    let(:bob) { Factory(:address_book, :name => "Bob Martin", :zipcode => 12345) }
+    let(:david) { Factory(:address_book, :name => "David Henderson", :zipcode => 12347) }
+    let(:zheimer) { Factory(:address_book, :name => "Clarke Zheimer", :zipcode => 12348) }
 
-    it "returns nil if the criteria is unknown" do
-      AddressBook.smart_filter({:name => {"magic" => "abracadabra"}}).should be_nil
+    before do
+      bob.save!
+      david.save!
+      zheimer.save!
+    end
+
+    it "returns an empty array if the criteria is unknown" do
+      AddressBook.smart_filter({:name => {"magic" => "abracadabra"}}).should == []
     end
 
     context "when the column to apply smart filtering is string or text" do
       context "when the criteria is 'contains'" do
 
         it "returns the record with the column that contains the given string" do
-          AddressBook.smart_filter({:name => {"contains" => "Bob"}}).each do |a|
-            a.name.should == bob.name
-            bob.name.should include(bob.name.split.first)
-          end
+          AddressBook.smart_filter({:name => {"contains" => "Bob"}}).first.name.should == bob.name
+          AddressBook.smart_filter({:name => {"contains" => "Bob"}}).first.name.should include(bob.name.split.first)
         end
 
       end
@@ -24,9 +29,7 @@ describe AddressBook do
       context "when the criteria is 'is'" do
 
         it "returns the record with the column of the exact given string" do
-          AddressBook.smart_filter({:name => {"is" => "Bob Martin"}}).each do |a|
-            a.name.should == bob.name
-          end
+          AddressBook.smart_filter({:name => {"is" => "Bob Martin"}}).first.name.should == bob.name
         end
 
       end
@@ -34,9 +37,7 @@ describe AddressBook do
       context "when the criteria is 'does_not_contain'" do
 
         it "returns the record with the column that does not contain the given string" do
-          AddressBook.smart_filter({:name => {"does_not_contain" => "Bob Martin"}}).each do |a|
-            a.name.should_not == bob.name
-          end
+          AddressBook.smart_filter({:name => {"does_not_contain" => "Bob Martin"}}).first.name.should_not == bob.name
         end
 
       end
@@ -44,9 +45,7 @@ describe AddressBook do
       context "when the criteria is starts_with" do
 
         it "returns the record with the column that starts with the given string" do
-          AddressBook.smart_filter({:name => {"starts_with" => "David"}}).each do |a|
-            a.name.should =~ /^David[.]*/
-          end
+          AddressBook.smart_filter({:name => {"starts_with" => "David"}}).first.name.should =~ /^David[.]*/
         end
 
       end
@@ -54,9 +53,7 @@ describe AddressBook do
       context "when the criteria is ends_with" do
 
         it "returns the record with the column that ends with the given string" do
-          AddressBook.smart_filter({:name => {"ends_with" => "Henderson"}}).each do |a|
-            a.name.should =~ /[.]*Henderson$/
-          end
+          AddressBook.smart_filter({:name => {"ends_with" => "Henderson"}}).first.name.should =~ /[.]*Henderson$/
         end
 
       end
@@ -64,7 +61,36 @@ describe AddressBook do
     end
 
     context "when the column to apply smart filtering is integer" do
-      
+
+      context "when the criteria is equals_to" do
+
+        it "returns records with the column that equals the given integer" do
+          AddressBook.smart_filter({:zipcode => {"equals_to" => "12345"}}).first.zipcode.should == 12345
+        end
+
+      end
+
+      context "when the criteria is greater_than" do
+        it "returns records with the column that is greater than the given integer" do
+          AddressBook.smart_filter({:zipcode => {"greater_than" => "12345"}}).first.zipcode.should > 12345
+        end
+      end
+
+      context "when the criteria is less_than" do
+        it "returns records with the column that is less than the given integer" do
+          AddressBook.smart_filter({:zipcode => {"less_than" => "12346"}}).first.zipcode.should < 12346
+        end
+      end
+
+      context "when the criteria is between" do
+        it "returns records with the column that is between the the given integers" do
+          AddressBook.smart_filter({:zipcode => {"between" => ["12343", "12348"]}}).should have(3).items
+          AddressBook.smart_filter({:zipcode => {"between" => ["12343", "12348"]}}).each do |contact|
+            contact.should satisfy { |c| c.zipcode >= 12343 && c.zipcode <= 12348 }
+          end
+        end
+      end
+
     end
 
     context "when the column to apply smart filtering is boolean" do
