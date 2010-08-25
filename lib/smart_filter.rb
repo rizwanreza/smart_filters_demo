d85210e05e775b9196d6bc36e227990bd8b5ca6e
Rizwan Reza <rizwanreza@gmail.com>
2010-08-25 05:23:13 +0430

    Created methods for all of the current criteria available.

--- lib/smart_filter.rb
+++ lib/smart_filter.rb
@@ -4,28 +4,17 @@ module SmartFilter
     columns.each do |column|
       if options[column.name.to_sym]
         case options[column.name.to_sym].keys.first
-        when "contains" then @conds << contains(column.name, options[column.name.to_sym]["contains"])
+        when "contains"         then @conds << contains(column.name, options[column.name.to_sym]["contains"])
         when "does_not_contain" then @conds << does_not_contain(column.name, options[column.name.to_sym]["does_not_contain"])
-        when "is"
-          return find(:all, 
-                      :conditions => ["#{column.name} = ?", "#{options[column.name.to_sym]["is"]}"])
-        when "starts_with"
-          return find(:all, 
-                      :conditions => ["#{column.name} LIKE ?", "#{options[column.name.to_sym]["starts_with"]}%"])
-        when "ends_with"
-          return find(:all, 
-                      :conditions => ["#{column.name} LIKE ?", "%#{options[column.name.to_sym]["ends_with"]}"])
-        when "equals_to"
-          return find(:all,
-                      :conditions => ["#{column.name} = ?", "#{options[column.name.to_sym]["equals_to"]}"])
-        when "greater_than"
-          return find(:all,
-                      :conditions => ["#{column.name} > ?", "#{options[column.name.to_sym]["greater_than"]}"])
-        when "less_than"
-          return find(:all,
-                      :conditions => ["#{column.name} < ?", "#{options[column.name.to_sym]["less_than"]}"])
-        when "between"
-          @conds << between(column.name, options[column.name.to_sym]["between"].first, options[column.name.to_sym]["between"].last)
+        when "is"               then @conds << is(column.name, options[column.name.to_sym]["is"])
+        when "starts_with"      then @conds << starts_with(column.name, options[column.name.to_sym]["starts_with"])
+        when "ends_with"        then @conds << ends_with(column.name, options[column.name.to_sym]["ends_with"])
+        when "equals_to"        then @conds << equals_to(column.name, options[column.name.to_sym]["equals_to"])
+        when "greater_than"     then @conds << greater_than(column.name, options[column.name.to_sym]["greater_than"])
+        when "less_than"        then @conds << less_than(column.name, options[column.name.to_sym]["less_than"])
+        when "between"          then @conds << between(column.name, 
+                                                      options[column.name.to_sym]["between"].first, 
+                                                      options[column.name.to_sym]["between"].last)
         else
           return []
         end
@@ -62,6 +51,28 @@ module SmartFilter
   end
 
   def between(column, start, finish)
-    ["#{column} BETWEEN ? AND ?", {"#{start}" => "#{finish}"}]
+    ["#{column} BETWEEN ? AND ?", {start => finish}]
+  end
+
+  def is(column, term)
+    ["#{column} = ?", term]
+  end
+
+  def starts_with(column, term)
+    ["#{column} LIKE ?", "#{term}%"]
+  end
+
+  def ends_with(column, term)
+    ["#{column} LIKE ?", "%#{term}"]
+  end
+
+  alias :equals_to :is
+
+  def greater_than(column, term)
+    ["#{column} > ?", term]
+  end
+  
+  def less_than(column, term)
+    ["#{column} < ?", term]
   end
 end
